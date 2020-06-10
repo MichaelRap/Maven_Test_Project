91bd5c97ef4a72b4244e8623497e676abda59bb0
Leo Li <leo.li08@sap.com>
2020-06-10 09:23:20 +0800

    update files.

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * ProjectName:
+ * FileName:
+ * Description:
+ * Copyright:Copyright(c)2017
+ * Company:SAP
+ *
+ * @authorSAP
+ * @date
+ * @see
+ * @since
+ */
+public class HelloWorld
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World...");
+    }
+}
